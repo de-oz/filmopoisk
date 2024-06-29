141fa0b05405a15d79dfd69462e9a19e1295f8a2
de-oz <101826623+de-oz@users.noreply.github.com>
2024-06-29 20:08:02 +0300

    feat: реализовать вывод списка фильмов и пагинацию

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
 import Layout from './components/Layout/Layout.jsx';
+import { useGetMoviesQuery } from './api/api.js';
+import { useState } from 'react';
 
 function App() {
+  const [page, setPage] = useState(1);
+  const { data, isLoading, isFetching } = useGetMoviesQuery(page);
+
   return (
     <>
       <Layout>
-        <main>Main</main>
+        <>
+          <ul>
+            {isLoading
+              ? 'LOADING...'
+              : isFetching
+                ? 'FETCHING DATA...'
+                : data.search_result.map((item) => <li key={item.id}>{item.title}</li>)}
+          </ul>
+          <button
+            onClick={() => setPage((prev) => prev - 1)}
+            disabled={page === 1}>
+            Previous
+          </button>
+          {page}
+          <button
+            onClick={() => setPage((prev) => prev + 1)}
+            disabled={page === data?.total_pages}>
+            Next
+          </button>
+        </>
       </Layout>
     </>
   );
---
+++ src/api/api.js
@@ -0,0 +1,15 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const apiUrl = 'http://localhost:3030/api/v1';
+
+export const apiSlice = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({ baseUrl: apiUrl }),
+  endpoints: (builder) => ({
+    getMovies: builder.query({
+      query: (page = 1) => `/search?page=${page}`,
+    }),
+  }),
+});
+
+export const { useGetMoviesQuery } = apiSlice;
